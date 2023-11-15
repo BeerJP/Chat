9a7d327d295b7f0ad9ff51f4d482c542135a8995
BeerJP <69442040+BeerJP@users.noreply.github.com>
2023-11-15 16:40:39 +0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
-import './assets/App.css';
+import './assets/app.css';
 import * as React from "react";
-import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Chatpage from "./pages/Chatpage";
 
 
 function App() {
   return (
-    <NextUIProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route key={1} path='/' element={<Chatpage/>}/>
-        </Routes>
-      </BrowserRouter>
-    </NextUIProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route key={1} path='/' element={<Chatpage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
