86f9d0339c36e6a3566094d94f9b5039a8a7fcba
BeerJP <scasifer@gmail.com>
2023-11-16 16:58:06 +0700

    Update 16/11/66 V.2

--- server/main.go
+++ server/main.go
@@ -1,28 +1,75 @@
 package main
 
 import (
-	database "server/database"
+	"fmt"
+	"time"
 
-	fiber "github.com/gofiber/fiber/v2"
-	cors "github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+var Db *gorm.DB
+
+type Messages struct {
+	Name string    `json:"name"`
+	Text string    `json:"text"`
+	Time time.Time `json:"time"`
+}
+
+// func getMessage(c *fiber.Ctx) error {
+// 	var message []Messages
+// 	result := Db.Find(&message)
+// 	return c.JSON()
+// }
+
+// func addMessage(c *fiber.Ctx) error {
+// 	messages := Messages{
+// 		Name: "Beer",
+// 		Text: "Hello",
+// 		Time: time.Now(),
+// 	}
+// 	result := Db.Create(&messages)
+// 	if result.Error != nil {
+// 		fmt.Println("failed...")
+// 	}
+// 	return nil
+// }
+
+func conn() {
+	Db, err := gorm.Open(mysql.Open("root:@(127.0.0.1)/full-stack-chat"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("MySQL Is Not Connected")
+	}
+	Db.AutoMigrate(&Messages{})
+}
 
 func main() {
 
-	chatDb := database.Database()
-	httpServer := fiber.New()
-	httpServer.Use(cors.New(cors.Config{
-		AllowOrigins:     "*",
-		AllowHeaders:     "*",
-		AllowMethods:     "*",
-		AllowCredentials: true,
-	}))
+	conn()
+	// app := fiber.New()
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     "*",
+	// 	AllowHeaders:     "*",
+	// 	AllowMethods:     "*",
+	// 	AllowCredentials: true,
+	// }))
 
-	httpServer.Get("/chat", func(c *fiber.Ctx) error {
-		result := database.Query(chatDb)
-		return c.SendString(result)
-	})
+	messages := Messages{
+		Name: "Beer",
+		Text: "Hello",
+		Time: time.Now(),
+	}
+	result := Db.Create(&messages)
+	if result.Error != nil {
+		fmt.Println("failed...")
+	}
 
-	httpServer.Listen(":8000")
+	// app.Get("/chat")
+	// app.Post("/message", addMessage)
+
+	// err := app.Listen(":8000")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 }
